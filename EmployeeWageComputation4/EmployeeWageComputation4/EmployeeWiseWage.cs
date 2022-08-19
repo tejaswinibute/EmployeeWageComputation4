0cabce920fa4da72940dc95baf409c177bf0fe55
tejaswinibute <tejaswinibute31@gmail.com>
2022-08-19 12:53:28 +0530

    [Tejaswini] Add Company wise Wage

---
+++ EmployeeWageComputation4/EmployeeWageComputation4/EmployeeWiseWage.cs
@@ -0,0 +1,61 @@
+﻿namespace EmployeeWageComputation4
+{
+    public class EmployeeWiseWage
+    {
+        public string CompanyName;
+        public byte WorkHoursFullDay;
+        public byte WorkHoursPartDay;
+        public byte WorkHoursPerMonth;
+        public byte WorkDaysPerMonth;
+        public int EmployeeWagePerHour;
+
+        public int CompanyDailyWage = 0;
+        public int CompanyMonthlyWage = 0;
+        public byte AttendanceChk;
+
+        public EmployeeWiseWage(string companyName, byte workHoursFullDay, byte workHoursPartDay, byte workHoursPerMonth, byte workDaysPerMonth, int employeeWagePerHour)
+        {
+            CompanyName = companyName;
+            WorkHoursFullDay = workHoursFullDay;
+            WorkHoursPartDay = workHoursPartDay;
+            WorkHoursPerMonth = workHoursPerMonth;
+            WorkDaysPerMonth = workDaysPerMonth;
+            EmployeeWagePerHour = employeeWagePerHour;
+        }
+
+        public int GetDailyWage()
+        {
+            var attendance = EmployeeAttendance.Attendance();
+
+
+
+            switch (attendance)
+            {
+                case "Present": CompanyDailyWage = WorkHoursFullDay * EmployeeWagePerHour; AttendanceChk = 2; break;
+                case "Part Present": CompanyDailyWage = WorkHoursPartDay * EmployeeWagePerHour; AttendanceChk = 1; break;
+                default: CompanyDailyWage = 0; break;
+            }
+            return CompanyDailyWage;
+        }
+
+        public int GetMonthlyWage()
+        {
+            var workHours = 0;
+            var days = 0;
+
+            while (workHours < WorkHoursPerMonth && days < WorkDaysPerMonth)
+            {
+                var dailyWage = GetDailyWage();
+                if (AttendanceChk == 2)
+                    workHours += WorkHoursFullDay;
+                else if (AttendanceChk == 1)
+                    workHours += WorkHoursPartDay;
+
+                CompanyMonthlyWage += dailyWage;
+                days++;
+            }
+
+            return CompanyMonthlyWage;
+        }
+    }
+}
---
+++ EmployeeWageComputation4/EmployeeWageComputation4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EmployeeWageComputation4
+{
+    public enum Variable : byte
+    {
+        EmployeePresent = 1,
+        EmployeePartPresent = 2,
+        WorkHoursFullDay = 8,
+        WorkHoursPartDay = 4,
+        WorkHoursPerMonth = 100,
+        WorkDaysPerMonth = 20,
+        EmployeeWagePerHour = 20
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n Welcome to Employee Wage Computation Program");
+
+            // UC 1 to 5
+
+            Console.WriteLine("\n >>>>>> Employee MonthlyWage Calculation <<<<<< ---- ${0}", MonthlyWage.MonthlyWageCalculation());
+
+            // UC 6 and 7 
+
+            Console.WriteLine(" >>>>>> Employee MonthlyWage Calculation With Hours or Days Completion Criteria <<<<<< ---- ${0} \n\n", MonthlyWageWithCriteria.MonthlyWageWithCriteriaCalculation()); // UC 6 and 7
+
+            //UC 8
+
+            Console.Write(" >>>> Employee Wise Monthly Wage <<<< \n\n Enter Company Name : "); var cName = Console.ReadLine();
+            Console.Write(" Enter Work hours a day : "); var hoursDay = Convert.ToByte(Console.ReadLine());
+            Console.Write(" Enter Work hours for HALF day : "); var hoursPartDay = Convert.ToByte(Console.ReadLine());
+            Console.Write(" Enter Maximum Work HOURS per month : "); var hoursMonth = Convert.ToByte(Console.ReadLine());
+            Console.Write(" Enter Maximum Work DAYS per month : "); var daysMonth = Convert.ToByte(Console.ReadLine());
+            Console.Write(" Enter Hourly Wage : "); var wage = Convert.ToByte(Console.ReadLine());
+            var company1 = new EmployeeWiseWage(cName, hoursDay, hoursPartDay, hoursMonth, daysMonth, wage);
+            Console.WriteLine("\n >>> Employee was paid $ {0} by {1} after completing a month", company1.GetMonthlyWage(), company1.CompanyName);
+        }
+    }
+}
+
