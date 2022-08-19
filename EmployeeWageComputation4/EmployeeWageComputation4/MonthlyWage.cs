c1e338bdb995c1d8c3113d454652450b667a68b6
tejaswinibute <tejaswinibute31@gmail.com>
2022-08-19 15:43:17 +0530

    [Tejaswini] Add Monnthly Wage With Criteria Program

---
+++ EmployeeWageComputation4/EmployeeWageComputation4/MonthlyWage.cs
@@ -0,0 +1,18 @@
+﻿namespace EmployeeWage
+{
+    public class MonthlyWage
+    {
+        public static int MonthlyWageCalculation()
+        {
+            var countDays = 0;
+            var monthlyWage = 0;
+            while (countDays < (byte)Variable.WorkDaysPerMonth)
+            {
+                var dailyWage = DailyWage.DailyWageCalculation();
+                monthlyWage += dailyWage;
+                countDays++;
+            }
+            return monthlyWage;
+        }
+    }
+}
---
+++ EmployeeWageComputation4/EmployeeWageComputation4/MonthlyWageWithCriteria.cs
@@ -0,0 +1,29 @@
+﻿namespace EmployeeWageComputation4
+{
+    public class MonthlyWageWithCriteria
+    {
+        public static int MonthlyWageWithCriteriaCalculation()
+        {
+            var workHours = 0;
+            var days = 0;
+            var monthlyWageWithCriteria = 0;
+
+            while (workHours < (byte)Variable.WorkHoursPerMonth && days < (byte)Variable.WorkDaysPerMonth)
+            {
+                var dailyWage = DailyWage.DailyWageCalculation();
+                if (dailyWage >= (byte)Variable.WorkHoursFullDay * (byte)Variable.EmployeeWagePerHour)
+                    workHours += (byte)Variable.WorkHoursFullDay;
+                else if (dailyWage < (byte)Variable.WorkHoursPartDay * (byte)Variable.EmployeeWagePerHour)
+                    workHours += (byte)Variable.WorkHoursPartDay;
+
+                monthlyWageWithCriteria += dailyWage;
+
+                days++;
+            }
+
+            return monthlyWageWithCriteria;
+        }
+    }
+}
+
+ 
