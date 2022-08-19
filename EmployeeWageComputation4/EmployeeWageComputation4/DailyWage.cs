b77cd7c808f2c68e5ec79014e01395f702301274
tejaswinibute <tejaswinibute31@gmail.com>
2022-08-19 13:01:31 +0530

    [Tejaswini] Add Daily Wage Program

---
+++ EmployeeWageComputation4/EmployeeWageComputation4/DailyWage.cs
@@ -0,0 +1,19 @@
+﻿namespace EmployeeWage
+{
+    public class DailyWage
+    {
+        public static int DailyWageCalculation()
+        {
+            int dailyWage;
+            var status = EmployeeAttendance.Attendance();
+
+            switch (status)
+            {
+                case "Present": dailyWage = (byte)Variable.WorkHoursFullDay * (byte)Variable.EmployeeWagePerHour; break;
+                case "Part Present": dailyWage = (byte)Variable.WorkHoursPartDay * (byte)Variable.EmployeeWagePerHour; break;
+                default: dailyWage = 0; break;
+            }
+            return dailyWage;
+        }
+    }
+}
