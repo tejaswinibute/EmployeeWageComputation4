a9deb9b283cd0e7f771154e45a519b0238dc2f76
tejaswinibute <tejaswinibute31@gmail.com>
2022-08-19 13:09:56 +0530

    [Tejaswini] Add Employee Attendance Program

---
+++ EmployeeWageComputation4/EmployeeWageComputation4/EmployeeAttendance.cs
@@ -0,0 +1,22 @@
+﻿namespace EmployeeWageComputation4
+{
+    public class EmployeeAttendance
+    {
+        public static string Attendance()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 3); // random numbers between 0 and 2, not including 2
+
+            string status;
+
+            if (empCheck == (byte)Variable.EmployeePresent)
+                status = "Present";
+            else if (empCheck == (byte)Variable.EmployeePartPresent)
+                status = "Part Present";
+            else
+                status = "Absent";
+
+            return status;
+        }
+    }
+}
